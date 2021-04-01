43eb51a622a761277bccbd4f6946d689583a4833
Chebesova Irina <viking724907@mail.ru>
2021-04-01 14:31:55 +0300

    small fix+add l3

--- chebesova/lab4/lab4.asm
+++ chebesova/lab4/lab4.asm
@@ -1,380 +1,298 @@
-TESTPC SEGMENT
-	ASSUME CS:TESTPC, DS:TESTPC, ES:NOTHING, SS:NOTHING
-	ORG 100H
-START: JMP BEGIN
+ASTACK SEGMENT STACK
+   DW 200 DUP(?)
+ASTACK ENDS
 
-;----------------------------
-AVAILABLE_MEMORY db 'Available Memory <Bytes>:$'
-EXTENDED_MEMORY db 'Extended Memory <KBytes>:$'
-MCB_LIST db 'MCB  List:', 0DH, 0AH, '$'
-MCB_LIST_NUMBER db 'MCB @  $'
-MCB_LIST_SIZE db 'Size:      $'
-MCB_LIST_ADDRESS db 'Address:      $'
-MCB_LIST_SC_SD db 'SC/SD:  $'
+DATA SEGMENT
+    already_load_str db 'Interruption was already loaded', 0DH, 0AH, '$'
+    success_load_str db 'Loading of interruption went successfully', 0DH, 0AH, '$'
+    not_load_str db 'Interruption isnt load', 0DH, 0AH, '$'
+    restored_str db 'Interruption is restored now', 0DH, 0AH, '$'
+DATA ENDS
 
-PSP_TYPE db 'PSP TYPE:  $'
-PSP_FREE db 'Free PSP             $'
-PSP_EXCLUDED_HIGH_DRIVER_MEM db'Excluded high driver $'
-PSP_OS_XMS_UMB db 'Belongs to OS XMS UMB$'
-PSP_MS_DOS db 'Belong MS DOS        $'
-PSP_OCCUPIED_386MAX_UMB db 'Occupied mem by 386MAX UMB$'
-PSP_BLOCKED_386MAX db 'Blocked 386MAX        $'
-PSP_BELONGS_386MAX db 'Belongs 386MAX        $'
+CODE SEGMENT
+    ASSUME CS:CODE, DS:DATA, SS:ASTACK
 
-DEFAULT_TYPE db '                     $'
+;------------------------------------
+    PRINT_MESSAGE  PROC  NEAR
+		push AX
+		mov AH, 9
+		int 21h
+		pop AX
+		ret
+	PRINT_MESSAGE  ENDP
+;------------------------------------
 
-;----------------------------
-TETR_TO_HEX PROC near
-    and AL, 0Fh
-    cmp AL, 09
-    jbe NEXT
-    add AL, 07
-NEXT:
-    add AL, 30h
-    ret
-TETR_TO_HEX ENDP
+;------------------------------------
+    SET_CURSOR PROC NEAR
+        mov ah, 02h
+        mov bh, 0h
+        mov dh, 0h
+        mov dl, 0h
+        int 10h
+        ret
+    SET_CURSOR ENDP
 
-BYTE_TO_HEX PROC near
-    push CX
-    mov AH, AL
-    call TETR_TO_HEX
-    xchg AL, AH
-    mov CL, 4
-    shr AL, CL
-    call TETR_TO_HEX
-    pop CX
-    ret
-BYTE_TO_HEX ENDP
+    GET_CURSOR PROC NEAR
+        mov ah, 03h
+        mov bh, 0
+        int 10h
+        ret
+    GET_CURSOR ENDP
+;------------------------------------
 
-WRD_TO_HEX PROC near
-    push BX
-    mov BH, AH
-    call BYTE_TO_HEX
-    mov [DI], AH
-    dec DI
-    mov [DI], AL
-    dec DI
-    mov AL, BH
-    call BYTE_TO_HEX
-    mov [DI], AH
-    dec DI
-    mov [DI], AL
-    pop BX
-    ret
-WRD_TO_HEX ENDP
+;------------------------------------
+    MY_INT PROC FAR
+        jmp begin_proc
+        counter db 'Interruption counter: 0000$'
+        signature dw 7777h
+        keep_ip dw 0
+        keep_cs dw 0
+        keep_ss dw 0
+        keep_sp dw 0
+        keep_ax dw 0
+		address_of_psp dw ?
+        my_stack dw 16 dup(?)
 
-BYTE_TO_DEC PROC near
-    push CX
-    push DX
-    xor AH, AH
-    xor DX, DX
-    mov CX, 10
-loop_bd:
-    div CX
-    or DL, 30h
-    mov [SI], DL
-    dec SI
-    xor DX, DX
-    cmp AX, 10
-    jae loop_bd
-    cmp AL, 00h
-    je end_l
-    or AL, 30h
-    mov [SI], AL
-end_l:
-    pop DX
-    pop CX
-    ret
-BYTE_TO_DEC ENDP
+    begin_proc:
+        mov keep_sp, SP
+        mov keep_ax, AX
+        mov AX, SS
+        mov keep_ss, AX
+        mov AX, keep_ax
+        mov SP, offset begin_proc
+        mov AX, seg my_stack
+        mov SS, AX
+        push AX 
+        push CX 
+        push DX
 
-;-----------------------
-PRINT_MESSAGE  PROC  NEAR
-    push AX
-    mov AH, 9
-    int 21h
-    pop AX
-    ret
-PRINT_MESSAGE  ENDP
+        call GET_CURSOR
+        push DX
+        call SET_CURSOR
+        push SI
+	    push CX
+	    push DS
+   	    push BP
+        mov AX, seg counter
+    	mov DS, AX
+    	mov SI, offset counter
+    	add SI, 21
+       	mov CX, 4
 
-PRINT_MESSAGE_BYTE  PROC  NEAR
-    push AX
-    mov AH, 02h
-    int 21h
-    pop AX
-    ret
-PRINT_MESSAGE_BYTE  ENDP
+    loop_for_count:
+        mov BP, CX
+        mov AH, [SI+BP]
+        inc AH
+        mov [SI+BP], AH
+        cmp AH, 3ah
+        jne for_print
+        mov AH, 30h
+        mov [SI+BP], AH
+       	loop loop_for_count
 
-PRINT_EOF PROC NEAR
-    push AX
-    push DX
-    mov DL, 0dh
-    call PRINT_MESSAGE_BYTE
-    mov DL, 0ah
-    call PRINT_MESSAGE_BYTE
-    pop DX
-    pop AX
-    ret
-PRINT_EOF ENDP
+    for_print:
+       	pop BP
+       	pop DS
+       	pop CX
+       	pop SI
+    	push ES
+    	push BP
+    	mov AX, seg counter
+    	mov ES,AX
+    	mov AX, offset counter
+    	mov BP,AX
+    	mov AH, 13h 
+    	mov AL, 00h
+    	mov CX, 26
+    	mov BH,0
+    	int 10h
+    	pop BP
+    	pop ES
+    	pop DX
+    	mov AH,02h
+    	mov BH,0h
+    	int 10h
 
-PRINT_AV_MEMORY PROC NEAR
-    push AX
-    push BX
-    push CX
-    push DX
-    push DI
-    xor CX, CX
-    mov BX, 010h
-    mul BX
-    mov DI, DX
-    mov BX, 0ah
+        pop DX 
+        pop CX 
+        pop AX 
+        mov keep_ax, AX
+        mov SP, keep_sp
+        mov AX, keep_ss
+        mov SS, AX
+        mov AX, keep_ax
+        mov AL, 20h	
+        out 20h, AL	
+        iret
+    my_int_last:
+    MY_INT ENDP
+;------------------------------------
 
-loop_for_division_1:
-    div BX
-    push DX
-    xor DX, DX
-    inc CX
-    cmp AX, 0h
-    jne loop_for_division_1
+;------------------------------------
+    CHECK_UPLOAD_KEY PROC NEAR
+       	push AX
+        push BP
+        mov CL, 0h
+        mov BP, 81h
+       	mov AL, ES:[BP + 1]
+       	cmp AL, '/'
+       	jne exit
+       	mov AL, ES:[BP + 2]
+       	cmp AL, 'u'
+       	jne exit
+       	mov AL, ES:[BP + 3]
+       	cmp AL, 'n'
+       	jne exit
+       	mov CL, 1h
 
-loop_for_print_symbol_1:
-    pop DX
-    add DL, 30h
-    call PRINT_MESSAGE_BYTE
-    loop loop_for_print_symbol_1
-	
-    pop DI
-    pop DX
-    pop CX
-    pop BX
-    pop AX
-    ret
-PRINT_AV_MEMORY ENDP
+    exit:
+        pop BP
+       	pop AX
+       	ret
+    CHECK_UPLOAD_KEY ENDP
 
-PRINT_EX_MEMORY PROC NEAR
-    push AX
-    push BX
-    push CX
-    push DX
-    push DI
-    xor CX, CX
-    xor DX, DX
-    mov BX, 0ah
 
-loop_for_division_2:
-    div BX
-    push DX
-    xor DX, DX
-    inc CX
-    cmp AX, 0h
-    jne loop_for_division_2
+    IF_ALREADY_LOAD PROC NEAR
+        push AX
+        push DX
+        push ES
+        push SI
+        mov CL, 0h
+        mov AH, 35h
+        mov AL, 1ch
+        int 21h
+        mov SI, offset signature
+        sub SI, offset MY_INT
+        mov DX, ES:[BX+SI]
+        cmp DX, signature
+        jne if_end
+        mov CL, 1h 
 
-loop_for_print_symbol_2:
-    pop DX
-    add DL, 30h
-    call PRINT_MESSAGE_BYTE
-    loop loop_for_print_symbol_2
+    if_end:
+        pop SI
+        pop ES
+        pop DX
+        pop AX
+        ret
+    IF_ALREADY_LOAD ENDP
 
-    pop DI
-    pop DX
-    pop CX
-    pop BX
-    pop AX
-    ret
-PRINT_EX_MEMORY ENDP
 
-;-----------------------
-TASK_1_1 PROC NEAR
-    push ax
-    push bx
-    push dx
-    mov dx, offset AVAILABLE_MEMORY
-    call PRINT_MESSAGE
-    mov ah, 4ah
-    mov bx, 0ffffh
-    int 21h
-    mov ax, bx
-    call PRINT_AV_MEMORY
-    call PRINT_EOF
-    pop dx
-    pop bx
-    pop ax
-    ret
-TASK_1_1 ENDP
+    LOAD PROC NEAR
+       	push AX
+        push CX
+       	push DX
+       	call IF_ALREADY_LOAD
+       	cmp CL, 1h
+       	je already_load
+        mov address_of_psp, ES
+       	mov AH, 35h
+    	mov AL, 1ch
+    	int 21h
+        mov keep_cs, ES
+	    mov keep_ip, BX
+    	push ES
+        push BX
+       	push DS
+       	lea DX, MY_INT
+       	mov AX, seg MY_INT
+       	mov DS, AX
+       	mov AH, 25h
+       	mov AL, 1ch
+       	int 21h
+       	pop DS
+        pop BX
+        pop ES
+        mov DX, offset success_load_str
+       	call PRINT_MESSAGE
+       	lea DX, my_int_last
+       	mov CL, 4h
+       	shr DX, CL
+       	inc DX 
+       	add DX, 100h
+       	xor AX,AX
+       	mov AH, 31h
+       	int 21h
+        jmp end_load
 
-TASK_1_2 PROC NEAR
-    push AX
-    push BX
-    push DX
-    mov AL, 30h
-    out 70h, AL
-    in AL, 71h
-    mov bl, AL
-    mov AL, 31h
-    out 70h, AL
-    in AL, 71h
-    mov BH, AL
-    mov DX, offset EXTENDED_MEMORY
-    call PRINT_MESSAGE
-    mov AX, BX
-    call PRINT_EX_MEMORY
-    call PRINT_EOF
-    pop DX
-    pop BX
-    pop AX
-    ret
-TASK_1_2 ENDP
+    already_load:
+     	mov DX, offset already_load_str
+        call PRINT_MESSAGE
 
-PRINT_MCB PROC near
-    push AX
-    push DX
-    push DI
-    mov DX, offset PSP_TYPE
-    call PRINT_MESSAGE
-    cmp AX, 0000h
-    je print_free
-    cmp AX, 0006h
-    je print_OS_XMS_UMB
-    cmp AX, 0007h
-    je print_excluded_high_driver_mem
-    cmp AX, 0008h
-    je print_MS_DOS
-    cmp AX, 0FFFAh
-    je print_occupied_386MAX_UMB
-    cmp AX, 0FFFDh
-    je print_blocked_386MAX
-    cmp AX, 0FFFEh
-    je print_belongs_386MAX_UMB
-    jmp print_default
+    end_load:
+       	pop DX
+        pop CX
+       	pop AX
+       	ret
+    LOAD ENDP
 
-print_free:
-    mov DX, offset PSP_FREE
-    call PRINT_MESSAGE
-    jmp end_print
 
-print_OS_XMS_UMB:
-    mov DX, offset PSP_OS_XMS_UMB
-    call PRINT_MESSAGE
-    jmp end_print
+    UNLOAD PROC NEAR
+       	push AX
+       	push SI
+       	call IF_ALREADY_LOAD
+       	cmp CL, 1h
+       	jne not_load
+        cli
+        push DS
+        push ES
+        mov AH, 35h
+        mov AL, 1ch
+        int 21h
+        mov SI, offset keep_ip
+	    sub SI, offset MY_INT
+	    mov DX, ES:[BX+SI]
+	    mov AX, ES:[BX+SI+2]
+   	    mov DS, AX
+        mov AH, 25h
+        mov AL, 1ch
+        int 21h
+        mov AX, ES:[BX+SI+4]
+   	    mov ES, AX
+   	    push ES
+       	mov AX, ES:[2ch]
+       	mov ES, AX
+       	mov AH, 49h
+       	int 21h
+       	pop ES
+       	mov AH, 49h
+       	int 21h
+        pop ES
+        pop DS
+        sti
+        mov DX, offset restored_str
+       	call PRINT_MESSAGE
+        jmp end_unload
 
-print_excluded_high_driver_mem:
-    mov DX, offset PSP_EXCLUDED_HIGH_DRIVER_MEM
-    call PRINT_MESSAGE
-    jmp end_print
+    not_load:
+        mov DX, offset not_load_str
+        call PRINT_MESSAGE
 
-print_MS_DOS:
-    mov DX, offset PSP_MS_DOS
-    call PRINT_MESSAGE
-    jmp end_print
+    end_unload:
+       	pop SI
+       	pop AX
+       	ret
+    UNLOAD ENDP
+;------------------------------------
 
-print_occupied_386MAX_UMB:
-    mov DX, offset PSP_OCCUPIED_386MAX_UMB
-    call PRINT_MESSAGE
-    jmp end_print
+;------------------------------------
+    MAIN PROC FAR
+       	mov AX, DATA
+       	mov DS, AX
+        call CHECK_UPLOAD_KEY
+        cmp CL, 0h
+        jne upload_key
+        call LOAD
+        jmp end_main
 
-print_blocked_386MAX:
-    mov DX, offset PSP_BLOCKED_386MAX
-    call PRINT_MESSAGE
-    jmp end_print
+    upload_key:
+        call UNLOAD
 
-print_belongs_386MAX_UMB:
-    mov DX, offset PSP_BELONGS_386MAX
-    call PRINT_MESSAGE
-    jmp end_print
+    end_main:
+        xor AL, AL
+        mov AH, 4ch
+        int 21h
+    MAIN ENDP
 
-print_default:
-    mov DI, offset DEFAULT_TYPE
-    add DI, 3
-    call WRD_TO_HEX
-    mov DX, offset DEFAULT_TYPE
-    call PRINT_MESSAGE
+CODE ENDS
 
-end_print:
-    pop DI
-    pop DX
-    pop AX
-    ret
-PRINT_MCB ENDP
-
-TASK_1_3 PROC NEAR
-    push AX
-    push BX
-    push DX
-    push CX
-    push SI
-    push DI
-    call PRINT_EOF
-    mov DX, offset MCB_LIST
-    call PRINT_MESSAGE
-    mov AH, 52h
-    int 21h
-    mov AX, ES:[BX-2]
-    mov ES, AX
-    xor CX, CX
-    mov CL, 01h
-
-loop_for_mcb:
-    mov AL, CL
-    mov SI, offset MCB_LIST_NUMBER
-    add SI, 5
-    call BYTE_TO_DEC
-    mov DX, offset MCB_LIST_NUMBER
-    call PRINT_MESSAGE
-    mov AX, ES
-    mov DI, offset MCB_LIST_ADDRESS
-    add DI, 12
-    call WRD_TO_HEX
-    mov DX, offset MCB_LIST_ADDRESS
-    call PRINT_MESSAGE
-    mov AX, ES:[1]
-    call PRINT_MCB
-    mov AX, ES:[3]
-    mov DI, offset MCB_LIST_SIZE
-    add DI, 9
-    call WRD_TO_HEX
-    mov DX, offset MCB_LIST_SIZE
-    call PRINT_MESSAGE
-    mov BX, 8
-    mov DX, offset MCB_LIST_SC_SD
-    call PRINT_MESSAGE
-    push CX
-    mov CX, 7
-	
-    loop_for_print_sc_sd:
-        mov DL, ES:[BX]
-        call PRINT_MESSAGE_BYTE
-        inc BX
-        loop loop_for_print_sc_sd
-		
-    call PRINT_EOF
-    pop CX
-    mov AH, ES:[0]
-    cmp AH, 5ah
-    je end_task_1_3
-    mov BX, ES:[3]
-    inc BX
-    mov AX, ES
-    add AX, BX
-    mov ES, AX
-    inc CL
-    jmp loop_for_mcb
-
-end_task_1_3:
-    pop DI
-    pop SI
-    pop CX
-    pop DX
-    pop BX
-    pop AX
-    ret
-TASK_1_3 ENDP
-
-BEGIN:
-    call TASK_1_1
-    call TASK_1_2
-    call TASK_1_3
-    xor AL, AL
-    mov AH, 4ch
-    int 21h
-
-TESTPC  ENDS
-        END START
+END MAIN
